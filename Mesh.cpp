35a676f8f96e2dfc756920281a9158669b04471f
Jason Henderson <jasonalanhenderson@gmail.com>
2025-01-27 15:58:40 -0500

    Creation of buffers and initialization of meshes.

---
+++ Mesh.cpp
@@ -0,0 +1,58 @@
+#include "Mesh.h"
+#include <d3d11.h>
+#include <wrl/client.h>
+#include "Graphics.h"
+#include "Vertex.h"
+
+//Constructor to create both the vertex and index buffer
+Mesh::Mesh(Vertex* vertices, size_t numVertices, unsigned int* indices, size_t numIndices)
+{
+	//Took inspiration from the demo code for passing in the number of vertices and indices
+	//Thought to use sizeof(arr)/sizeof(arr[0]) but would not be correct as it would include size of a pointer and not the full array
+	//Initialization of both integer counts
+	this->numVertices = (unsigned int)numVertices;
+	this->numIndices = (unsigned int)numIndices;
+
+	//Creation of vertex buffer
+	{
+		//Vertex buffer description
+		D3D11_BUFFER_DESC vertexDescription = {};
+		vertexDescription.Usage = D3D11_USAGE_IMMUTABLE;
+		vertexDescription.ByteWidth = sizeof(Vertex) * (unsigned int)numVertices;
+		vertexDescription.BindFlags = D3D11_BIND_VERTEX_BUFFER;
+		vertexDescription.CPUAccessFlags = 0;
+		vertexDescription.MiscFlags = 0;
+		vertexDescription.StructureByteStride = 0;
+
+		//Structure to hold the vertex data
+		D3D11_SUBRESOURCE_DATA initialVertexData = {};
+		initialVertexData.pSysMem = vertices;
+
+		//Create the vertex buffer
+		Graphics::Device->CreateBuffer(&vertexDescription, &initialVertexData, vertexBuffer.GetAddressOf());
+	}
+
+	//Creation of index buffer
+	{
+		//Index buffer description
+		D3D11_BUFFER_DESC indexDescription = {};
+		indexDescription.Usage = D3D11_USAGE_IMMUTABLE;
+		indexDescription.ByteWidth = sizeof(unsigned int) * (unsigned int)numIndices;
+		indexDescription.BindFlags = D3D11_BIND_INDEX_BUFFER;
+		indexDescription.CPUAccessFlags = 0;
+		indexDescription.MiscFlags = 0;
+		indexDescription.StructureByteStride = 0;
+
+		//Structure to hold initial index data
+		D3D11_SUBRESOURCE_DATA initialIndexData = {};
+		initialIndexData.pSysMem = indices;
+
+		//Create the index buffer
+		Graphics::Device->CreateBuffer(&indexDescription, &initialIndexData, indexBuffer.GetAddressOf());
+	}
+}
+
+Mesh::~Mesh()
+{
+	//Can be empty, as everything is handled through smart pointers
+}
---
+++ Mesh.h
@@ -0,0 +1,36 @@
+#pragma once
+#include <d3d11.h>
+#include <wrl/client.h>
+#include "Vertex.h"
+
+//Class that creates both index and vertex buffers for a mesh
+//Mesh will be allowed to use both buffers created, meaning it will be able to draw the geometry using the buffers
+class Mesh
+{
+public:
+	//Constructor
+	Mesh(Vertex* vertices, size_t numVertices, unsigned int* indices, size_t numIndices);
+
+	//Destructor
+	~Mesh();
+
+	//Functions to return vertex and index buffer
+	Microsoft::WRL::ComPtr<ID3D11Buffer> GetVertexBuffer();
+	Microsoft::WRL::ComPtr<ID3D11Buffer> GetIndexBuffer();
+
+	//Functions to return the count of vertices and indices
+	unsigned int GetIndexCount();
+	unsigned int GetVertexCount();
+	
+	//Draw function to set the buffers and draw the geometry
+	void Draw();
+
+private:
+	//ComPtrs for vertex and index buffer
+	Microsoft::WRL::ComPtr<ID3D11Buffer> vertexBuffer;
+	Microsoft::WRL::ComPtr<ID3D11Buffer> indexBuffer;
+
+	//Integers for vertices and indices
+	unsigned int numVertices;
+	unsigned int numIndices;
+};
